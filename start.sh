69bf2524a19d2df1a517741739f717ac94970a35
vvmruder <vvmruder@users.noreply.github.com>
2023-03-27 09:19:58 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,6 @@
 
 [[ $DEBUG == "1" ]] && env
 
+nginx
+
 /usr/bin/xvfb-run --auto-servernum --server-num=1 /usr/bin/spawn-fcgi -P /run/qgis.pid -p 9993 -n -d /home/qgis -- /usr/lib/cgi-bin/qgis_mapserv.fcgi
